e76e78a3510d3870d8dffa63ebd479c7e80bf7a3
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-05-02 21:05:56 +0500

    feat: matrix-elements-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,9 +18,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let result = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        result += matrix[i][j];
+      }
+      if ((matrix[i][j] !== 0) && (matrix[i + 1])) {
+        result += matrix[i + 1][j];
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
