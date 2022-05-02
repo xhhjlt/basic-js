eb5b8a1efeb5b6b6eaeb600d905aba56348770da
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-05-02 16:48:52 +0500

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,13 +22,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isReverse = !isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if ((key === undefined) || (message === undefined))
+      throw {
+        name: "IncorrectArgumentsError",
+        message: "Incorrect arguments!",
+      }
+    let messageArr = message.toUpperCase().split("");
+    if (this.isReverse) messageArr.reverse();
+    for (let i = 0, j = 0; i < messageArr.length; i++) {
+      if ((messageArr[i].charCodeAt(0) >= 65) && (messageArr[i].charCodeAt(0) <= 90)) {
+        messageArr[i] = String.fromCharCode((messageArr[i].charCodeAt(0) + key.toUpperCase().charCodeAt(j)) % 26 + 65)
+        j++;
+      }
+      if (j === key.length) j = 0;
+    }
+    return messageArr.join('');
+  }
+  decrypt(encryptedMessage, key) {
+    if ((key === undefined) || (encryptedMessage === undefined))
+      throw {
+        name: "IncorrectArgumentsError",
+        message: "Incorrect arguments!",
+      }
+    let messageArr = encryptedMessage.toUpperCase().split("");
+    if (this.isReverse) messageArr.reverse();
+    for (let i = 0, j = 0; i < messageArr.length; i++) {
+      if ((messageArr[i].charCodeAt(0) >= 65) && (messageArr[i].charCodeAt(0) <= 90)) {
+        messageArr[i] = String.fromCharCode((messageArr[i].charCodeAt(0) - key.toUpperCase().charCodeAt(j) + 26) % 26 + 65)
+        j++;
+      }
+      if (j === key.length) j = 0;
+    }
+    return messageArr.join('');
   }
 }
 
